0e3bd9f38a5be67c612adb11ee164e389f17a9e4
zidane-arkan <zidark897@Gmail.com>
2023-03-18 15:30:37 +0700

    Update Hooks and Basic Form V1.3

--- src/hooks/use-input-basic.jsx
+++ src/hooks/use-input-basic.jsx
@@ -1,29 +1,43 @@
 import React, { useState, useReducer } from 'react';
 
+const initialValue = {
+    inputValue: '',
+    isInputBlured: false
+};
+const inputStateReducer = (state, action) => {
+    if (action.type === 'INPUT_VALUE') {
+        return { inputValue: action.value, isInputBlured: state.isInputBlured };
+    } else if (action.type === 'INPUT_BLUR') {
+        return { isInputBlured: true, inputValue: state.inputValue };
+    } else if (action.type === 'INPUT_RESET') {
+        return {
+            inputValue: '',
+            isInputBlured: false
+        };
+    }
+    return initialValue;
+};
 const useInputBasic = (inputCondition) => {
-    //Get Input Value
-    const [inputValue, setInputValue] = useState('');
-    //Is Input Field Blured
-    const [isInputBlured, setisInputBlured] = useState(false);
+    const [inputState, dispatchInput] = useReducer(inputStateReducer, initialValue);
+    //Extract State
+    const { inputValue, isInputBlured } = inputState;
     //Check Validation For Input
     const isInputValidate = inputCondition(inputValue);
     const isUserChanged = !isInputValidate && isInputBlured;
+    const inputValidation = isUserChanged ? 'form-control invalid' : 'form-control ';
     //Change Status Input According To user Activity
     // console.log(isInputValidate);
-    const inputValidation = isUserChanged ? 'form-control invalid' : 'form-control ';
-
     const inputValueHandler = (event) => {
         // console.log(isInputValidate);
-        setInputValue(event.target.value);
+        dispatchInput({ type: 'INPUT_VALUE', value: event.target.value });
     };
     const inputBlurHandler = () => {
         // console.log(1);
-        setisInputBlured(true);
+        dispatchInput({ type: 'INPUT_BLUR', value: true });
     };
     //Reset Value
     const resetInput = () => {
-        setInputValue('');
-        setisInputBlured(false);
+        dispatchInput({ type: 'INPUT_RESET' });
     };
     return { resetInput, inputValue, isInputValidate, isInputBlured, isUserChanged, inputValueHandler, inputBlurHandler, inputValidation };
 };
